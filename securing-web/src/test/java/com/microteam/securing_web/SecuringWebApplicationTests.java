9ce0572c19fb26da03e4af2547473dd3f7984207
yugal-nandurkar <yugalnandurkar5@gmail.com>
2024-10-30 16:29:36 +0530

    Securing a Web Application

---
+++ securing-web/src/test/java/com/microteam/securing_web/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microteam.securing_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
